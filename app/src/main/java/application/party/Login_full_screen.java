1bd62a896f0c7ff028a1145f3e0d9d7c568186ae
adrian <adalgme@inf.upv.es>
2014-10-26 21:25:35 +0100

    First Commit--guias de programacion no mayusculas utiliza_esto en vez de UtilizaEsto.

--- app/src/main/java/application/party/Login_full_screen.java
+++ app/src/main/java/application/party/Login_full_screen.java
@@ -14,14 +14,16 @@ import application.party.util.SystemUiHider;
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user wwinteraction.
- *ssposdkopaskdopaskd
+ * ssposdkopaskdopaskd
+ *
  * @see SystemUiHider
  */
 public class Login_full_screen extends Activity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */df
+     */
+    df
     private static final boolean AUTO_HIDE = true;
 
     /**
@@ -29,18 +31,36 @@ public class Login_full_screen extends Activity {
      * user interaction before hiding the system UI.
      */
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
     /**
      * If set, will toggle the system UI visibility upon interaction. Otherwise,
      * will show the system UI visibility upon interaction.
      */
     private static final boolean TOGGLE_ON_CLICK = true;
-
     /**
      * The flags to pass to {@link SystemUiHider#getInstance}.
      */
     private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
+    Handler mHideHandler = new Handler();
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mSystemUiHider.hide();
+        }
+    };
     /**
      * The instance of the {@link SystemUiHider} for this activity.
      */
@@ -124,30 +144,6 @@ public class Login_full_screen extends Activity {
         // are available.
         delayedHide(100);
     }
-
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-        }
-    };
 
     /**
      * Schedules a call to hide() in [delay] milliseconds, canceling any
